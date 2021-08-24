c1c6eb41ceb814bed146f50ced65bbf04212ddb0
moon-i <tjans1102@gmail.com>
2021-08-24 10:44:59 +0900

    [MLA-18] network 모듈 mapper 없이 사용하도록 수정
    Retrofit을 suspend function으로 사용하고, mapper를 제거합니다
    
    refs - https://github.com/moon-i/MiniLoginApplication/issues/18

--- app/src/main/java/com/study/miniloginapplication/data/datasource/UserDataSourceImpl.kt
+++ app/src/main/java/com/study/miniloginapplication/data/datasource/UserDataSourceImpl.kt
@@ -7,11 +7,14 @@ package com.study.miniloginapplication.data.datasource
 //  Created by moon on 06/08/2021
 //
 
+import com.google.gson.Gson
 import com.study.miniloginapplication.data.spec.ResponseUsers
-import com.study.network.RequestMapper
+import com.study.network.ServiceBuilder
 
 class UserDataSourceImpl : UserDataSource {
     override suspend fun getUserList(size: Int): ResponseUsers? {
-        return RequestMapper().getUsers(size, ResponseUsers::class.java)
+        val gson = Gson()
+        val service = ServiceBuilder.buildService()
+        return gson.fromJson(service.getUsers(size), ResponseUsers::class.java)
     }
 }
